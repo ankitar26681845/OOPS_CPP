0e893e9fbb367e330d462157e3d5b3eb68af8e15
Ankit kumar <56801657+ankitar26681845@users.noreply.github.com>
2023-06-20 23:14:26 +0530

    Create Class_CPP.cpp

---
+++ Class_CPP.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+class Father{
+    public:
+    int age;
+    string name;
+    
+};
+int main(){
+    Father fatobj;
+    fatobj.age =45;
+    fatobj.name="Jagdip singh";
+    cout<<fatobj.age;
+    return 0;
+}
